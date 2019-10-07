9ea3d2d30926b63807690188c389d5c88c92a980
Cole Mooney <colemooney@Coles-MacBook-Pro.local>
2019-10-07 15:35:15 -0400

    Updates

---
+++ precache-manifest.790b7dfbfee7fdd6766a31b536d5d6b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f91c46a514268840b3dc",
+    "url": "://colemooney.github.io/lab-wiki-countries.git/static/js/runtime~main.f91c46a5.js"
+  },
+  {
+    "revision": "c67b964c465e882e030a",
+    "url": "://colemooney.github.io/lab-wiki-countries.git/static/js/main.c67b964c.chunk.js"
+  },
+  {
+    "revision": "d8c38291839b3ff02fa6",
+    "url": "://colemooney.github.io/lab-wiki-countries.git/static/js/1.d8c38291.chunk.js"
+  },
+  {
+    "revision": "c67b964c465e882e030a",
+    "url": "://colemooney.github.io/lab-wiki-countries.git/static/css/main.c3d8efdd.chunk.css"
+  },
+  {
+    "revision": "d8c38291839b3ff02fa6",
+    "url": "://colemooney.github.io/lab-wiki-countries.git/static/css/1.8d086993.chunk.css"
+  },
+  {
+    "revision": "4de73d59c2351a1c0e6bf429ac0c08dc",
+    "url": "://colemooney.github.io/lab-wiki-countries.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "://colemooney.github.io/lab-wiki-countries.git/precache-manifest.790b7dfbfee7fdd6766a31b536d5d6b0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("://colemooney.github.io/lab-wiki-countries.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
